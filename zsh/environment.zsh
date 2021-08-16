624ea1c2f611122bf63dbedbe49153b4c353dca7
Penpen7 <Raosys@live.jp>
2021-08-17 02:19:55 +0900

    環境変数の並び替えと、不要なものを削除

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -2,12 +2,17 @@
 #
 # export TERM="xterm-256color"
 export LC_ALL=ja_JP.UTF-8
+export GOPATH="$HOME/go"
+export GOBIN="${GOPATH}/bin"
+
+export PATH="/bin":$PATH
+export PATH="/usr/bin":$PATH
+export PATH="/usr/local/bin":$PATH
+export PATH="$GOPATH/bin":$PATH
+export PATH="$HOME/.nodebrew/current/bin":$PATH
 export PATH="/usr/local/opt/gnu-time/libexec/gnubin:$PATH"
 export PATH="/usr/local/Cellar/llvm/10.0.0_3/bin/:$PATH"
 export PATH="/usr/local/Cellar/ytop/0.6.2/bin/ytop":$PATH
-export PATH="$HOME/.nodebrew/current/bin":$PATH
-export EDITOR=vim
-export GOBIN=${GOPATH}bin
 export LDFLAGS="${LDFLAGS} -L/usr/local/opt/zlib/lib"
 export LDFLAGS="${LDFLAGS} -L/usr/local/lib"
 export LDFLAGS="${LDFLAGS} -L/usr/local/opt/openssl@1.1/lib"
@@ -19,6 +24,6 @@ export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/openssl@1.1/include"
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/opt/zlib/lib/pkgconfig"
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/pkgconfig"
 export FZF_DEFAULT_OPS="--extended"
-export CPATH=$CPATH:/usr/local/include:/Users/naoki/.pyenv/versions/3.6.3/include/python3.6m
+
 export EDITOR=nvim
 export GIT_EDITOR=nvim
