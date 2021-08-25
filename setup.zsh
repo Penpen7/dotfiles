0ea9c77b69fa78a8b6c2e9775788c3ea6a4602b0
Penpen7 <Raosys@live.jp>
2021-08-26 02:30:08 +0900

    setup.zshにzshenv設置追加

--- setup.zsh
+++ setup.zsh
@@ -4,6 +4,7 @@
 REPOSITORY_PATH=$(cd $(dirname $0); pwd)
 ln -sf $REPOSITORY_PATH/zsh/.zshrc $HOME
 ln -sf $REPOSITORY_PATH/tmux/.tmux.conf $HOME
+ln -sf $REPOSITORY_PATH/zsh/.zshenv
 
 # nvim
 NVIM_CONFIG=$HOME/.config/nvim
