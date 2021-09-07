0affce15cdaac12fe217ac43fa8e037e537921fc
Penpen7 <Raosys@live.jp>
2021-09-08 06:23:40 +0900

    tigrcの設定が適用されるようにした

--- setup.zsh
+++ setup.zsh
@@ -5,6 +5,7 @@ REPOSITORY_PATH=$(cd $(dirname $0); pwd)
 ln -sf $REPOSITORY_PATH/zsh/.zshrc $HOME
 ln -sf $REPOSITORY_PATH/tmux/.tmux.conf $HOME
 ln -sf $REPOSITORY_PATH/zsh/.zshenv $HOME
+ln -sf $REPOSITORY_PATH/tig/.tigrc $HOME
 
 # nvim
 NVIM_CONFIG=$HOME/.config/nvim
