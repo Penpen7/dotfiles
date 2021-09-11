d9459b5b8ae53a1d1797485be7e4b4c49ac73e77
Penpen7 <Raosys@live.jp>
2021-09-12 04:21:16 +0900

    履歴表示修正

--- fzf/zsh.zsh
+++ fzf/zsh.zsh
@@ -44,5 +44,5 @@ function ch() {
     "select substr(title, 1, $cols), url
       from urls order by last_visit_time desc" |
   awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' | 
-  fzf --ansi --multi --preview-window down:0 | sed 's#.*\(https*://\)#\1#' | xargs $open > /dev/null 2> /dev/null
+  fzf --reverse --no-hscroll --ansi --multi --preview-window down:0 | sed 's#.*\(https*://\)#\1#' | xargs $open > /dev/null 2> /dev/null
 }
