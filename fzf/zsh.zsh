1b3a835f8f4c8781a172322c8056b6edcfd6dc9b
Penpen7 <Raosys@live.jp>
2021-09-12 04:07:55 +0900

    Chromeの履歴からブラウザを開くようにした

--- fzf/zsh.zsh
+++ fzf/zsh.zsh
@@ -26,3 +26,23 @@ function peco-cdr () {
         zle accept-line
     fi
 }
+
+function ch() {
+  local cols sep google_history open
+  cols=$(( COLUMNS / 3 ))
+  sep='{::}'
+
+  if [ "$(uname)" = "Darwin" ]; then
+    google_history="$HOME/Library/Application Support/Google/Chrome/Default/History"
+    open=open
+  else
+    google_history="$HOME/.config/google-chrome/Default/History"
+    open=xdg-open
+  fi
+  \cp -f "$google_history" /tmp/googlechrome_fzf
+  sqlite3 -separator $sep /tmp/googlechrome_fzf \
+    "select substr(title, 1, $cols), url
+      from urls order by last_visit_time desc" |
+  awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' | 
+  fzf --ansi --multi --preview-window down:0 | sed 's#.*\(https*://\)#\1#' | xargs $open > /dev/null 2> /dev/null
+}
