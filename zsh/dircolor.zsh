9b30c54335f9c1a951e78d54377de87264a6de6b
Penpen7 <Raosys@live.jp>
2021-09-24 01:45:43 +0900

    preztoのインストール消去

--- zsh/dircolor.zsh
+++ zsh/dircolor.zsh
@@ -7,7 +7,3 @@ eval `dircolors $HOME/.dircolors-solarized/dircolors.256dark`
 if [ -n "$LS_COLORS" ]; then
     zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 fi
-
-if [ ! -e $HOME/.zprezto ]; then
-  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-fi
