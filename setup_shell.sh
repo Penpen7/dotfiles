92fd54212d5f75cb9036e635cccf820350f209ef
Penpen7 <Raosys@live.jp>
2022-05-15 03:46:46 +0900

    preztoが動かなかったので修正

--- setup_shell.sh
+++ setup_shell.sh
@@ -11,7 +11,8 @@ ln -sf $REPOSITORY_PATH/zsh/.zshrc $HOME
 ln -sf $REPOSITORY_PATH/tmux/.tmux.conf $HOME
 ln -sf $REPOSITORY_PATH/zsh/.zshenv $HOME
 ln -sf $REPOSITORY_PATH/tig/.tigrc $HOME
-ln -sf $REPOSITORY_PATH/preztorc/zpreztorc $HOME/.preztorc
+ln -sf $REPOSITORY_PATH/preztorc/zpreztorc $HOME/.zpreztorc
+ln -sf $REPOSITORY_PATH/prezto $HOME/.zprezto
 ln -sf $REPOSITORY_PATH/hyper/.hyper.js $HOME/.hyper.js
 ln -sf $REPOSITORY_PATH/git/gitconfig $HOME/.gitconfig
 ln -sf $REPOSITORY_PATH/powerlevel/.p10k.zsh $HOME
