5353b9a1cb8a626d888414d0b5413dfa18e403ab
Penpen7 <Raosys@live.jp>
2022-08-21 01:55:42 +0900

    スクリプトが実行されるよう修正

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ REPOSITORY_PATH=$(
   pwd
 )
 scripts=(make_symlink.sh setup_brew.sh setup_node.sh setup_vim.sh)
-for i in ${!scripts[@]}; do
+for i in ${scripts[@]}; do
   ${REPOSITORY_PATH}/scripts/${i}
 done
