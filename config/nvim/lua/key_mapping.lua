66eafea9d1056b25463f218511bc3573eb10a651
Penpen7 <Raosys@live.jp>
2023-06-25 17:22:16 +0900

    うまく動作しない部分を削除

--- config/nvim/lua/key_mapping.lua
+++ config/nvim/lua/key_mapping.lua
@@ -5,7 +5,6 @@ vim.api.nvim_set_keymap("n", "<Space>t", ":bo terminal", { silent = true, norema
 vim.api.nvim_set_keymap("n", "<Space>q", ":q<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap("n", "<F4>", ":<C-u>setlocal relativenumber!<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap("", "<ESC><ESC>", ":nohl<CR>", {})
-vim.api.nvim_set_keymap("n", "x", '"_x', {})
 vim.api.nvim_set_keymap("n", "j", "gj", {})
 vim.api.nvim_set_keymap("n", "k", "gk", {})
 vim.api.nvim_set_keymap("n", "n", "nzz", {})
@@ -45,17 +44,5 @@ vim.cmd([[
     autocmd!
     autocmd BufRead * if line("'\"") > 0 && line("'\"") <= line("$") |
     \ exe "normal g`\"" | endif
-  augroup END
-]])
-vim.cmd([[
-  augroup vimrc-auto-mkdir
-    autocmd!
-    autocmd BufWritePre * call v:lua.auto_mkdir(expand('<afile>:p:h'), v:cmdbang)
-    function! s:auto_mkdir(dir, force)
-      if !isdirectory(a:dir) && (a:force ||
-      \    input(printf('"%s" does not exist. Create? [y/N]', a:dir)) =~? '^y\%[es]$')
-        call mkdir(iconv(a:dir, &encoding, &termencoding), 'p')
-      endif
-    endfunction
   augroup END
 ]])
