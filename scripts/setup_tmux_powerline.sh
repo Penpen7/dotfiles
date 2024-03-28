5ab317452ee1c7cee11cfd4816a8b46583602f9b
Penpen7 <Raosys@live.jp>
2024-03-29 00:13:38 +0900

    powerlineの管理方法を変更

--- scripts/setup_tmux_powerline.sh
+++ scripts/setup_tmux_powerline.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-pip3 install powerline-status
-pip3 install powerline-mem-segment
-pip3 show -f powerline-status | grep "powerline.conf$"
-mkdir -p ~/.config/powerline/{colorschemes,themes}/tmux
-
 REPOSITORY_PATH=$(
   cd $(dirname $0)
   git rev-parse --show-toplevel
 )
 
-ln -s "$REPOSITORY_PATH/config/powerline/tmux/config.json" ~/.config/powerline/config.json
-ln -s "$REPOSITORY_PATH/config/powerline/tmux/colors.json" ~/.config/powerline/colors.json
-ln -s "$REPOSITORY_PATH/config/powerline/tmux/tmux-theme.json" ~/.config/powerline/themes/tmux/tmux-theme.json
-ln -s "$REPOSITORY_PATH/config/powerline/colorscheme.json" ~/.config/powerline/colorschemes/tmux/tmux-colorscheme.json
+rm -rf $REPOSITORY_PATH/config/powerline/venv
+mkdir -p $REPOSITORY_PATH/config/powerline/venv
+python3 -m venv $REPOSITORY_PATH/config/powerline/venv
+source $REPOSITORY_PATH/config/powerline/venv/bin/activate
+which pip3
+pip3 install powerline-status
+pip3 install powerline-mem-segment
+deactivate
+
