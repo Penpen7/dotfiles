f0bee745cdaa565f954c847c8a309584a1622e5a
Penpen7 <Raosys@live.jp>
2024-07-07 18:51:10 +0900

    tablineからbufferlineを移行

--- config/nvim/lua/core/lazy.lua
+++ config/nvim/lua/core/lazy.lua
@@ -308,35 +308,23 @@ lazy.setup({
   },
   "nvim-tree/nvim-web-devicons",
   {
-    "kdheepak/tabline.nvim",
-    event = { "InsertEnter", "CursorHold", "FocusLost", "BufRead", "BufNewFile" },
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-      require("tabline").setup({
-        -- Defaults configuration options
-        enable = true,
-        options = {
-          -- If lualine is installed tabline will use separators configured in lualine by default.
-          -- These options can be used to override those settings.
-          section_separators = { "", "" },
-          component_separators = { "", "" },
-          max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
-          show_tabs_always = false,    -- this shows tabs only when there are more than one tab or if the first tab is named
-          show_devicons = true,        -- this shows devicons in buffer section
-          show_bufnr = false,          -- this appends [bufnr] to buffer section,
-          show_filename_only = false,  -- shows base filename only instead of relative path in filename
-          modified_icon = "+ ",        -- change the default modified icon
-          modified_italic = false,     -- set to true by default; this determines whether the filename turns italic if modified
-          show_tabs_only = false,      -- this shows only tabs instead of tabs + buffers
-        },
+      local highlights = require("nord").bufferline.highlights({
+        italic = true,
+        bold = true,
+        fill = "#181c24"
       })
-      if vim.fn.has("gui_running") == 1 then
-        if pcall(has_guioptions) then
-          vim.opt.guioptions:remove("e")
-        end
-      end
-      vim.opt.sessionoptions:append("tabpages,globals")
-    end,
-    requires = { { "hoob3rt/lualine.nvim", opt = true }, { "kyazdani42/nvim-web-devicons", opt = true } },
+
+      require("bufferline").setup({
+        options = {
+          separator_style = "slant",
+        },
+        highlights = highlights,
+      })
+    end
   },
   {
     'neoclide/coc.nvim',
