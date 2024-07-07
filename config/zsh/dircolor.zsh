8a0d05b91965bc4e9a1734aa35eb71ec8c3c83a0
Penpen7 <Raosys@live.jp>
2024-07-07 18:56:08 +0900

    dircolorsをnordに変更

--- config/zsh/dircolor.zsh
+++ config/zsh/dircolor.zsh
@@ -1,7 +1,7 @@
 # /bin/zsh
 #
 local sourcedirectory=$(dirname $0)
-eval `dircolors ${sourcedirectory}/grubox.dircolors`
+eval `dircolors ${sourcedirectory}/nord.dircolors`
 if [ -n "$LS_COLORS" ]; then
     zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 fi
