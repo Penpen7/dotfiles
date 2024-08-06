53b3f3d917381d877ef3436692feec33d2b995e2
Penpen7 <Raosys@live.jp>
2024-08-07 02:19:53 +0900

    cocのlistが不便だったので、telescope-coc.nvimを使用するようにした

--- config/nvim/lua/core/lazy/fzf.lua
+++ config/nvim/lua/core/lazy/fzf.lua
@@ -4,7 +4,10 @@ return (
     {
       "nvim-telescope/telescope.nvim",
       tag = "0.1.4",
-      dependencies = "nvim-lua/plenary.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        'fannheyward/telescope-coc.nvim',
+      },
       keys = { "<leader>f" },
       config = function()
         local builtin = require('telescope.builtin')
@@ -14,6 +17,26 @@ return (
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>fgc', builtin.git_commits, {})
         vim.keymap.set('n', '<leader>fgs', builtin.git_status, {})
+
+        local telescope = require("telescope")
+
+        telescope.setup({
+          extensions = {
+            coc = {
+              theme = 'ivy',
+              prefer_locations = true,    -- always use Telescope locations to preview definitions/declarations/implementations etc
+              push_cursor_on_edit = true, -- save the cursor position to jump back in the future
+              timeout = 3000,             -- timeout for coc commands
+            }
+          },
+        })
+        telescope.load_extension('coc')
+
+        -- GoTo code navigation
+        vim.keymap.set("n", "gd", ":Telescope coc definitions<CR>", { silent = true })
+        vim.keymap.set("n", "gy", ":Telescope coc type_definitions<CR>", { silent = true })
+        vim.keymap.set("n", "gi", ":Telescope coc implementations<CR>", { silent = true })
+        vim.keymap.set("n", "gr", ":Telescope coc references<CR>", { silent = true })
       end
     },
   }
