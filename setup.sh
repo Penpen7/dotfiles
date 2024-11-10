6bcbd76e93742ad952d4774e2daa5010f77b47b0
Penpen7 <Raosys@live.jp>
2024-11-10 22:25:28 +0900

    修正

--- setup.sh
+++ setup.sh
@@ -5,7 +5,11 @@ REPOSITORY_PATH=$(
   cd $(dirname $0)
   pwd
 )
-scripts=(make_symlink.sh setup_brew.sh setup_node.sh setup_vim.sh)
+
+./make_symlink.sh
+source "$HOME/.zshenv"
+
+scripts=(setup_brew.sh setup_node.sh setup_vim.sh)
 for i in ${scripts[@]}; do
   ${REPOSITORY_PATH}/scripts/${i}
 done
