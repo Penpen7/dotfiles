03a99c241b0811a9953bb25e17095629f816460e
Penpen7 <Raosys@live.jp>
2025-04-27 00:27:06 +0900

    avante.nvimの追加

--- config/nvim/lua/core/lazy/edit.lua
+++ config/nvim/lua/core/lazy/edit.lua
@@ -40,18 +40,18 @@ return {
     end,
   },
   {
-    "github/copilot.vim",
-    event = "VimEnter",
+    "zbirenbaum/copilot.lua",
+    event = { "InsertEnter" },
     config = function()
-      vim.g.copilot_filetypes = { markdown = true, gitcommit = true, yaml = true }
-    end
+      require("copilot").setup()
+    end,
   },
   {
     {
       "CopilotC-Nvim/CopilotChat.nvim",
       branch = "main",
       dependencies = {
-        { "github/copilot.vim" },
+        { "zbirenbaum/copilot.lua" },
         { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
       },
       opts = {
@@ -60,6 +60,61 @@ return {
       },
       -- See Commands section for default commands if you want to lazy load on them
     },
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "copilot",
+      auto_suggestions_provider = "copilot",
+
+      -- 動作設定
+      behaviour = {
+        auto_suggestions = false,
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+        minimize_diff = true,
+      },
+
+      -- ウィンドウ設定
+      windows = {
+        position = "bottom", -- サイドバーの位置
+        wrap = true,         -- テキストの折り返し
+        width = 30,          -- サイドバーの幅
+      },
+    },
+    build = "make",
+    -- 依存関係の設定
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",        -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+    }
   },
   { "dhruvasagar/vim-table-mode", event = { "InsertEnter" } },
   {
