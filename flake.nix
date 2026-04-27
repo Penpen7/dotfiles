418e9532b623146e121a5b67d9e111db888fb680
Penpen7 <Raosys@live.jp>
2026-04-27 18:08:07 +0900

    add nixfmt

--- flake.nix
+++ flake.nix
@@ -1,44 +1,45 @@
 {
-    description = "A flake to provision my environment";
+  description = "A flake to provision my environment";
 
-    inputs = {
-        nixpkgs = {
-            url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
-        };
-
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        nix-darwin = {
-            url = "github:LnL7/nix-darwin";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     };
 
-    outputs = {
-        self,
-        nixpkgs,
-        home-manager,
-        nix-darwin,
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nix-darwin,
     }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
     in
-      {
-        homeConfigurations."naoki" = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [./config/home-manager/home.nix];
+    {
+      homeConfigurations."naoki" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./config/home-manager/home.nix ];
+      };
+      darwinConfigurations = {
+        "uehara-mac" = nix-darwin.lib.darwinSystem {
+          specialArgs = { inherit self; };
+          modules = [
+            ./config/nix-darwin/configuration.nix
+          ];
         };
-        darwinConfigurations = {
-            "uehara-mac" = nix-darwin.lib.darwinSystem {
-                specialArgs = { inherit self; };
-                modules = [
-                   ./config/nix-darwin/configuration.nix
-                ];
-            };
-        };
+      };
     };
 }
